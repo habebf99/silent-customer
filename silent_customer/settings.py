3df63414c977249532e8451f5f45216ed3c165ff
Habeeb F. <HABEBF99@HOTMAIL.COM>
2025-05-27 02:27:35 +0300

    Initial project setup

--- silent_customer/settings.py
+++ silent_customer/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%4d1t70563%dq76ljd5ag^2bm%af$#k-tbv%(j$wezmepm(s%k'
-
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key-for-local-use')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
